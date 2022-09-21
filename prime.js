153a21fa17cf464513283304829296dfa30ca308
Achintya Singh <achintyasingh15@gmail.com>
2022-09-21 23:07:18 +0530

    corrected

--- prime.js
+++ prime.js
@@ -1,8 +1,8 @@
 
-function prime(num){
+function prime(number){
     let factor=0;
-     for(let i=1; i<=num;i++){
-           if (num%i==0){
+     for(let i=1; i<=number;i++){
+           if (number%i==0){
             factor++
            }
            if(factor==2){
