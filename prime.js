6a54adad11bc91f19578264875bd81e1a981397d
JAI RAJ SINGH <jairajsingh28july@gmail.com>
2022-09-21 21:47:31 +0530

    code_fixed

--- prime.js
+++ prime.js
@@ -1,17 +1,33 @@
 
-function prime(num){
-    let factor=0;
-     for(let i=1; i<=num;i++){
-           if (num%i==0){
-            factor++
-           }
-           if(factor==2){
-            return true;
-           }
-     }return false;
-}
+// function prime(num){
+//     let factor=0;
+//      for(let i=1; i<=num;i++){
+//            if (num%i==0){
+//             factor++
+//            }
+//            if(factor==2){
+//             return true;
+//            }
+//      }return false;
+// }
 
-let a=prime(13);
-if(a==true){
-    console.log(a)
-}
+// let a=prime(13);
+// if(a==true){
+//     console.log(a)
+// }
+
+function checkprime(num){
+    if (num<=1){
+        return false;
+    }
+        for(let i=2;i<=num**0.5;i++){
+            if(num%i==0){
+                return false;
+            }
+        }return true;
+    }
+    
+    for(let j=0;j<=15;j++){
+        if(checkprime(j))
+        { console.log(j, "is a prime number")}
+    }
