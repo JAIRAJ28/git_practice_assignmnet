9be4f6a54c8599865fc6fbb0ec6313827bef913b
JAI RAJ SINGH <jairajsingh28july@gmail.com>
2022-09-21 21:58:23 +0530

    codefixed

---
+++ new.js
@@ -0,0 +1,15 @@
+function checkprime(num){
+    if (num<=1){
+        return false;
+    }
+        for(let i=2;i<=num**0.5;i++){
+            if(num%i==0){
+                return false;
+            }
+        }return true;
+    }
+    
+    for(let j=0;j<=15;j++){
+        if(checkprime(j))
+        { console.log(j, "is a prime number")}
+    }
